5c41e7761c1d1c02342c4cfe619102fc8f5f44ca
Nomfanelo Khuzwayo <157467563+NO-MM@users.noreply.github.com>
2025-11-12 09:23:06 +0200

    Enhance blog UI and styling: updated templates, added CSS, refined routing, and improved .gitignore

--- latest_blogs/blog/urls.py
+++ latest_blogs/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.my_blog_list, name='my_blog_list'),
     path('create/', views.blog_create, name='blog_create'),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
